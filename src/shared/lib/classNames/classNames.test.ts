dda891a7753895e82a291614fe10a0a95c564d2d
rgdzv <r_gadzhiomarov@mail.ru>
2023-06-18 21:08:23 +0300

    test: set jest config and add test for classNames

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,42 @@
+import { classNames } from './classNames'
+
 describe('classNames', () => {
-    test('test', () => {
-        expect(true).toBe(true)
+    test('with only first argument', () => {
+        expect(classNames('someClass')).toBe('someClass')
+    })
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2'
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected)
+    })
+
+    test('with mods (both - true)', () => {
+        const expected = 'someClass class1 class2 hovered scrollable'
+        expect(
+            classNames('someClass', { hovered: true, scrollable: true }, [
+                'class1',
+                'class2'
+            ])
+        ).toBe(expected)
+    })
+
+    test('with mods (1 - false)', () => {
+        const expected = 'someClass class1 class2 scrollable'
+        expect(
+            classNames('someClass', { hovered: false, scrollable: true }, [
+                'class1',
+                'class2'
+            ])
+        ).toBe(expected)
+    })
+
+    test('with mods (1 - undefined)', () => {
+        const expected = 'someClass class1 class2 scrollable'
+        expect(
+            classNames('someClass', { hovered: undefined, scrollable: true }, [
+                'class1',
+                'class2'
+            ])
+        ).toBe(expected)
     })
 })
