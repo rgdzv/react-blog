f5c212a815195d54eb5601ada93dad7555a8eb30
rgdzv <r_gadzhiomarov@mail.ru>
2023-08-02 18:18:52 +0300

    refactor: change approach of theme decorator

--- src/shared/lib/storybook/decorators/ThemeDecorator.tsx
+++ src/shared/lib/storybook/decorators/ThemeDecorator.tsx
@@ -1,36 +1,11 @@
 import { type Decorator } from '@storybook/react'
-import { ThemeProvider, useTheme } from 'app/providers/ThemeProvider'
-import { type FC, type ReactElement, type ReactNode, useEffect } from 'react'
-interface AppSimulationPropsInterface {
-    children: ReactNode
-    theme2: string
-}
-
-const AppSimulation: FC<AppSimulationPropsInterface> = ({
-    children,
-    theme2
-}): ReactElement => {
-    const { toggleTheme } = useTheme()
-
-    useEffect(() => {
-        toggleTheme()
-    }, [theme2])
-
-    return (
-        <div className={`app ${theme2}`} data-testid='app'>
-            {children}
-        </div>
-    )
-}
 
 export const ThemeDecorator: Decorator = (Story, context) => {
-    const theme2: string = context.globals.theme
+    const theme: string = context.globals.theme
 
     return (
-        <ThemeProvider>
-            <AppSimulation theme2={theme2}>
-                <Story />
-            </AppSimulation>
-        </ThemeProvider>
+        <div className={`app ${theme}`}>
+            <Story />
+        </div>
     )
 }
