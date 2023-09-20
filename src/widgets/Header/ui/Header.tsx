7af4d9d55dd85bac10da0dc28e97b5ef372f0098
rgdzv <r_gadzhiomarov@mail.ru>
2023-09-20 22:21:04 +0300

    refactor: change modal configuration

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -9,15 +9,11 @@ export const Header: FC = () => {
     const { t } = useTranslation()
 
     const signIn = t('Войти')
-    const close = t('Закрыть')
+
     return (
         <header className={styles.header}>
             <Button onClick={openModal}>{signIn}</Button>
-            <Modal open={isModalOpen} onClose={closeModal}>
-                <header></header>
-                <section></section>
-                <Button onClick={closeModal}>{close}</Button>
-            </Modal>
+            <Modal open={isModalOpen} closeModal={closeModal}></Modal>
         </header>
     )
 }
--- src/widgets/Modal/ui/Modal.tsx
+++ src/widgets/Modal/ui/Modal.tsx
@@ -3,53 +3,82 @@ import {
     type FC,
     useEffect,
     type MouseEvent,
-    type ReactNode
+    type ReactNode,
+    useState
 } from 'react'
 import styles from './Modal.module.scss'
+import { classNames } from 'shared/lib'
+import { Button } from 'shared/ui'
+import { useTranslation } from 'react-i18next'
 
 interface ModalPropsInterface {
     children: ReactNode
     open: boolean
-    onClose: () => void
+    closeModal: () => void
 }
 
-export const Modal: FC<ModalPropsInterface> = ({ open, onClose, children }) => {
+export const Modal: FC<ModalPropsInterface> = ({
+    open,
+    closeModal,
+    children
+}) => {
+    const [isClosing, setIsClosing] = useState(false)
     const dialogRef = useRef<HTMLDialogElement>(null)
+    const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
+    const { t } = useTranslation()
     const { current } = dialogRef
 
-    const onClick = (e: MouseEvent<HTMLDialogElement>): void => {
-        const dialogDimensions = current?.getBoundingClientRect()
+    const timeoutSection = (): void => {
+        setIsClosing(true)
+        timeoutRef.current = setTimeout(() => {
+            current?.close()
+            setIsClosing(false)
+        }, 150)
+    }
 
-        if (dialogDimensions !== undefined) {
-            if (
-                e.clientX < dialogDimensions.left ||
-                e.clientX > dialogDimensions.right ||
-                e.clientY < dialogDimensions.top ||
-                e.clientY > dialogDimensions.bottom
-            ) {
-                current?.close()
-            }
+    const onClickOutside = (e: MouseEvent<HTMLDialogElement>): void => {
+        if (e.target === current) {
+            timeoutSection()
         }
     }
+
+    const onClickCloseButton = (): void => {
+        timeoutSection()
+    }
+
+    const classNameFinal = classNames(styles.dialog, {
+        [styles.closing]: isClosing
+    })
+
+    const closeButtonName = t('Закрыть')
 
     useEffect(() => {
         if (current === undefined) return
 
         if (open) {
             current?.showModal()
-        } else {
-            current?.close()
+        }
+
+        return () => {
+            clearTimeout(timeoutRef.current)
         }
     }, [open, current])
 
     return (
         <dialog
             ref={dialogRef}
-            className={styles.dialog}
-            onClick={onClick}
-            onClose={onClose}
+            className={classNameFinal}
+            onClick={onClickOutside}
+            onClose={closeModal}
         >
-            {children}
+            <article className={styles.dialogContent}>
+                <div className={styles.dialogSection}>{children}</div>
+                <footer>
+                    <Button onClick={onClickCloseButton}>
+                        {closeButtonName}
+                    </Button>
+                </footer>
+            </article>
         </dialog>
     )
 }
