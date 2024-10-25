a1eb73506b4aa19b585d70d75576e784688c4fcd
rgdzv <r_gadzhiomarov@mail.ru>
2024-10-25 20:38:21 +0300

    fix: add title property declaration in svg declaration module

--- src/app/types/images/index.d.ts
+++ src/app/types/images/index.d.ts
@@ -1,6 +1,6 @@
 declare module '*.svg' {
     import { type FC, type SVGProps } from 'react'
-    const SVG: FC<SVGProps<SVGElement>>
+    const SVG: FC<SVGProps<SVGElement> & { title?: string }>
     export default SVG
 }
 
