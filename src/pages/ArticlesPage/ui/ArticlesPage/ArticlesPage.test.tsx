f9d5cf7d22c729687b09f1a2c6713b3ee2df24bc
rgdzv <r_gadzhiomarov@mail.ru>
2024-11-06 10:16:03 +0300

    fix: fix article view changer test to properly use matchmedia function

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.test.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.test.tsx
@@ -3,6 +3,20 @@ import { render, screen } from '@testing-library/react'
 import { StoreProvider } from 'app/providers/StoreProvider'
 import ArticlesPage from './ArticlesPage'
 
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn()
+    }))
+})
+
 describe('ArticlesPage', () => {
     test('render', () => {
         render(
